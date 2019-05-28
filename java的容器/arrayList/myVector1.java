d9d8cf369bbc8dfb2472395d77993ea96c8b78aa
startwangming <46195718+startwangming@users.noreply.github.com>
2019-05-28 13:36:17 +0800

    Add files via upload

---
+++ java的容器/arrayList/myVector1.java
@@ -0,0 +1,12 @@
+package test9;
+
+import java.util.List;
+import java.util.Vector;
+
+public class myVector1 {
+	public static void main(String[] args) {
+		List<String> a = new Vector<>();
+		a.add("sss");
+		System.out.println(a);
+	}
+}
